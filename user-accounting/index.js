c20764be67a5f0c4a5e6b0f0c3904c83d3dc058e
Radu Achim <radu.achim@iteam.se>
2019-04-23 17:31:36 +0200

    parse qr code for debug purpose

--- user-accounting/index.js
+++ user-accounting/index.js
@@ -2,10 +2,16 @@ const express = require('express')
 const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
-
+const fs = require('fs')
 const port = 8900
 const chokidar = require('chokidar')
 const receipts = []
+const QrCode = require('qrcode-reader')
+
+const jimp = require('jimp')
+const util = require('util')
+const readFile = util.promisify(fs.readFile)
+const jimpRead = util.promisify(jimp.read)
 
 const watcher = chokidar.watch(`${__dirname}/assets`, {
   ignored: /^\./,
@@ -13,9 +19,24 @@ const watcher = chokidar.watch(`${__dirname}/assets`, {
 })
 
 watcher
-  .on('add', function(path) {
-    receipts.push(path.replace(`${__dirname}/assets`, ''))
-    io.emit('receipt', path.replace(`${__dirname}/assets`, ''))
+  .on('add', async function(path) {
+    const imgPath = path.replace(`${__dirname}/assets`, '')
+    const imgBuffer = await readFile(`${__dirname}/assets/${imgPath}`)
+    const image = await jimpRead(imgBuffer)
+    const qr = new QrCode()
+    qr.callback = function(err, value) {
+      if (err) {
+        console.error(err)
+        // TODO handle error
+      }
+      const receipt = {
+        img: imgPath,
+        receipt: value.result
+      }
+      receipts.push(receipt)
+      io.emit('receipt', receipt)
+    }
+    qr.decode(image.bitmap)
   })
   .on('change', function(path) {
     console.log('File', path, 'has been changed')
