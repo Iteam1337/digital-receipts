6704a1a98a9bb41b3d2b4badfddfa46946cd6e9b
Radu Achim <radu.achim@iteam.se>
2019-06-03 15:48:09 +0200

    test passing project to docker-build

---
+++ bin/docker-build.sh
@@ -0,0 +1,19 @@
+#!/bin/env bash
+# cd ..
+# docker build -f ca/Dockerfile -t digitalreceipts/ca .
+# docker push digitalreceipts/ca
+
+# docker build -f hash-registry/Dockerfile -t digitalreceipts/hash-registry .
+# docker push digitalreceipts/hash-registry
+
+# docker build -f shop/Dockerfile -t digitalreceipts/shop .
+# docker push digitalreceipts/shop
+
+# docker build -f user-accounting/Dockerfile -t digitalreceipts/user-accounting .
+# docker push digitalreceipts/user-accounting
+
+# docker build -f user-email/Dockerfile -t digitalreceipts/user-email .
+# docker push digitalreceipts/user-email
+
+
+echo $1
