d7ac2fe848cf6605d4d25a6f52e61d70ff98d33c
Radu Achim <radu.achim@iteam.se>
2019-06-11 19:51:23 +0200

    combine jobs and matrix try #9

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/env bash
 
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv ./kubectl /usr/local/bin/kubectl
+
 if [[ $1 == *"examples/poc"* ]]; then
   PROJECT_PATH="${1/examples\/poc\//}"
   echo "$PROJECT_PATH is being deployed"
