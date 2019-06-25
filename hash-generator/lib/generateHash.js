a17ef7046797211517fbb3484074fdc7e7edd023
Radu Achim <radu.achim@iteam.se>
2019-06-25 14:17:52 +0200

    fix: 🐛 url friendly hashes

--- hash-generator/lib/generateHash.js
+++ hash-generator/lib/generateHash.js
@@ -5,6 +5,9 @@ function generateHash(payload) {
     .createHash('sha256')
     .update(JSON.stringify(payload))
     .digest('base64')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_')
+    .replace(/=/g, '')
 }
 
 module.exports = generateHash
